5d45d8f40556308535843438fb0a25d6b83ac5c9
Diego Villalpando <30503143+Dialvive@users.noreply.github.com>
2020-09-21 10:34:40 -0500

    Update Pop-OS.sh

--- Pop-OS.sh
+++ Pop-OS.sh
@@ -23,7 +23,7 @@ sudo ufw default deny incoming &&
 sudo ufw default allow outgoing &&
 sudo ufw allow http &&
 sudo ufw allow https &&
-sudo ufw allow 'OpenSSH' &&
+sudo ufw allow ssh &&
 sudo ufw enable &&
 sudo ufw status verbose;
 
